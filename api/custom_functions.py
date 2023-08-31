60cfb9782c5cdf681a057359ced19de779d84a4d
PyTem <temni23@yandex.ru>
2023-08-31 13:42:18 +0700

    Fixed PEP8

--- api/custom_functions.py
+++ api/custom_functions.py
@@ -16,9 +16,7 @@ def draw_ticket_to_pdf(ticket: Ticket) -> bytes:
     rendered_html = render_to_string('ticket_pdf.html',
                                      context={'ticket': ticket})
 
-    pdf = weasyprint.HTML(string=rendered_html).write_pdf()
-
-    return pdf
+    return weasyprint.HTML(string=rendered_html).write_pdf()
 
 
 def get_text_email(user: User, ticket: Ticket) -> str:
@@ -26,23 +24,21 @@ def get_text_email(user: User, ticket: Ticket) -> str:
     name = user.get_full_name() if user.is_authenticated else 'Дорогой друг'
     link_main = 'ticketera.ru'  # Заглушка
     link_tickets = 'ticketera.ru/my_tickets'  # Заглушка
-    message = (f'Здравствуйте, {name}!<br>'
-               f'Во вложении письма находятся '
-               f'билеты из вашего заказа {ticket.id}<br>'
-               f'{ticket.event.name}\n{ticket.event.description}'
-               f'{ticket.event.place.address}<br>'
-               f'{ticket.event.date_event} {ticket.event.time_event}<br>'
-               f'Ряд: {ticket.row} Место {ticket.seat}<br>'
-               f'Для зарегистрированных пользователей сайта '
-               f'<a href="{link_main}">TICKETERA</a> билеты '
-               f'также доступны для скачивания в личном кабинете в '
-               f'разделе <a href="{link_tickets}">Мои билеты</a><br>'
-               f'Желаем вам хорошо провести время!<br><br>'
-               f'<b>Если письмо пришло вам по ошибке, просто '
-               f'проигнорируйте его</b>'
-               )
-
-    return message
+    return (f'Здравствуйте, {name}!<br>'
+            f'Во вложении письма находятся '
+            f'билеты из вашего заказа {ticket.id}<br>'
+            f'{ticket.event.name}\n{ticket.event.description}'
+            f'{ticket.event.place.address}<br>'
+            f'{ticket.event.date_event} {ticket.event.time_event}<br>'
+            f'Ряд: {ticket.row} Место {ticket.seat}<br>'
+            f'Для зарегистрированных пользователей сайта '
+            f'<a href="{link_main}">TICKETERA</a> билеты '
+            f'также доступны для скачивания в личном кабинете в '
+            f'разделе <a href="{link_tickets}">Мои билеты</a><br>'
+            f'Желаем вам хорошо провести время!<br><br>'
+            f'<b>Если письмо пришло вам по ошибке, просто '
+            f'проигнорируйте его</b>'
+            )
 
 
 def send_ticket_to_user(ticket: Ticket, user: User) -> None:
@@ -61,7 +57,7 @@ def send_ticket_to_user(ticket: Ticket, user: User) -> None:
     # Добавление вложения в письмо
     ticket = draw_ticket_to_pdf(ticket)
     attachment = MIMEApplication(ticket, Name="ticket")
-    attachment['Content-Disposition'] = f'attachment; filename="ticket.pdf"'
+    attachment['Content-Disposition'] = 'attachment; filename="ticket.pdf"'
     msg.attach(attachment)
 
     try:
