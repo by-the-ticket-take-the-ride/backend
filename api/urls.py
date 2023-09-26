037770646b1c3e1edb90aa1a90b183edb37f30c3
Artem Klimov <temni23@yandex.ru>
2023-09-26 20:44:16 +0700

    Fixed PEP8

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CityViewSet, EventViewSet, TicketViewSet, TypeEventViewSet,
-                    TypeHallViewSet, ZoneHallViewSet, FavoriteViewSet)
+from .views import (
+    CityViewSet, EventViewSet, TicketViewSet, TypeEventViewSet,
+    TypeHallViewSet, ZoneHallViewSet, FavoriteViewSet
+)
 
 app_name = 'api'
 
@@ -14,13 +16,12 @@ router.register('events', EventViewSet, basename='events')
 router.register('hall_types', TypeHallViewSet, basename='hall_types')
 router.register('zones_hall', ZoneHallViewSet, basename='zones_hall')
 router.register('tickets', TicketViewSet, basename='tickets')
-# router.register('favorite', FavoriteViewSet, basename='favotite')
+# router.register('favorite', FavoriteViewSet, basename='favorite')
 
 urlpatterns = [
     path('events/<int:id>/favorite/',
-         FavoriteViewSet.as_view({'post': 'create',
-                                  'delete': 'destroy',
-                                  }), name='favorite'),
+         FavoriteViewSet.as_view({'post': 'create', 'delete': 'destroy'}),
+         name='favorite'),
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
